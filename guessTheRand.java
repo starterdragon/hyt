a2dda2569cd044a9ea03b221d6a7fae556123f70
starterdragon <papadie102@gmail.com>
2018-09-23 22:18:19 +0800

    Update guessTheRand.java
    pfft

--- guessTheRand.java
+++ guessTheRand.java
@@ -4,33 +4,32 @@ public class Rndm{
 
 	static int tries = 3;
 	static int score = 0;
+
+	static int random;
 	
-	static boolean random = true;
-	static int number;
-	
-	public static void main(String[] args){
+	public static void main(String[] args)
+	{
 		Scanner reader = new Scanner (System.in);
+		randomInt();
 		core(reader);
 	}
 	
 	public static void core(Scanner in)
 	{
 	    
-        int ran = randomInt(random); //gets a random or existing number | (boolean random)
-        
-        //System.out.print("[debug] number is: "+ ran + "\n");
+        //System.out.print("[debug] number is: "+ random + "\n");
 	    System.out.print("\nScore:" + score + "\nChance(s):" + tries + "\nEnter Number: ");
 	    
 	    int guess = in.nextInt();
 
-	    if(guess == ran) //if the answer is correct
+	    if(guess == random) //if the answer is correct
 	    {
 	        score++; //add score
-	        random = true; //new number
+	        randomInt(); //new random number
 			core(in); //calls this function
 		} else {
 		    
-		    int check = ((guess > ran) ? 0 : 1); //returns 0 if it's lower or 1 if it's higher
+		    int check = ((guess > random) ? 0 : 1); //returns 0 if it's lower or 1 if it's higher
 		    
 			if(tries <= 1) //hack, fix being able to guess when there's 0 chance
 			{
@@ -40,7 +39,7 @@ public class Rndm{
 			        // calls core again and resets everything
 			        tries = 3;
 			        score = 0;
-			        random = true;
+			        randomInt();//new random number
 			        core(in);
 			    } else {
 			        System.out.println("Final Score: " + score); //returns final score
@@ -52,24 +51,20 @@ public class Rndm{
 			        case 0: System.out.println("> TIP: Lower <"); break;
 			    }
 			    tries--; //lessen try count
-			    random = false; //not random
 			    core(in); //calls this function
 			}
 		}
 	}
 	
-	public static int randomInt(boolean random)
+	public static int randomInt()
 	{
-	    if (random)
-	    {
-            int a = new Random().nextInt(10); //returns new random number
-                if(a == number)
-                {
-                    randomInt(true); //re-run if it is the same as the last one
-                }
-            return number = a; //returns and save the new number (buggy)
-	    } else {
-	        return number; //returns same number
-	    }
+        int a = new Random().nextInt(10); //returns new random number
+        
+        if(a == random)
+        {
+           randomInt(); //re-run if it is the same as the last one
+        }
+        
+        return random = a; //returns and save the new number (buggy)
 	}
 }
